6337ea50a92a6ff3dec905e98dc8878dc6a2a0f5
Shubhaankar Sharma <66492212+Shubhaankar-Sharma@users.noreply.github.com>
2021-10-07 16:52:55 +0530

    my dotfiles

---
+++ .config/i3lock/lock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+scrot /tmp/screen.png
+ICON=$HOME/.config/i3lock/lock.png
+TMPBG=/tmp/screen.png
+scrot /tmp/screen.png
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+i3lock -u -i $TMPBG
+rm /tmp/screen.png
